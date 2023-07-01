a3fe0127ed531257bdb2bcaac8d2e927d9b3ea25
Gaspard Deremble <g.deremble@laposte.net>
2023-07-01 15:08:35 +0200

    added lobby basics

--- client/Scripts/Scenes Scripts/Lobby.gd
+++ client/Scripts/Scenes Scripts/Lobby.gd
@@ -1,6 +1,9 @@
 extends Node
 
 
+signal change_scene (scene)
+
+
 #region Variables
 
 #exports variables
@@ -8,11 +11,11 @@ extends Node
 
 
 #scene objects
-
+onready var obj_nb_players_text: Label = $CanvasLayer/Nb_players_text
 
 
 #others
-
+var players_in_lobby := PoolIntArray()
 
 
 #unused variable
@@ -20,5 +23,35 @@ var _unused
 
 #endregion
 
-func _ready() -> void:
+func ready():
+	players_in_lobby.append(get_tree().get_network_unique_id())
+	rpc("player_joined_lobby")
+
+
+	return
+
+func _on_Back_button_pressed():
+	rpc("player_left_lobby")
+	emit_signal("change_scene", "Main_menu")
+
+	return
+
+
+puppet func get_lobby_info(players: PoolIntArray):
+	players_in_lobby.append_array(players)
+	obj_nb_players_text.text = "Number of players: " + str(players_in_lobby.size())
+
+	return
+
+remote func player_joined_lobby():
+	var id: int = get_tree().get_rpc_sender_id()
+	players_in_lobby.append(id)
+
+	return
+
+remote func player_left_lobby():
+	var id: int = get_tree().get_rpc_sender_id()
+	players_in_lobby.remove(players_in_lobby.find(id))
+	obj_nb_players_text.text = "Number of players: " + str(players_in_lobby.size())
+
 	return
--- server/Scripts/Lobby.gd
+++ server/Scripts/Lobby.gd
@@ -11,7 +11,7 @@ extends Node
 
 
 #others
-
+var players_in_lobby := PoolIntArray()
 
 
 #unused variable
@@ -20,3 +20,13 @@ var _unused
 #endregion
 
 
+master func player_joined_lobby():
+    var id: int = get_tree().get_rpc_sender_id()
+    players_in_lobby.append(id)
+    rpc_id(id, "get_lobby_info", players_in_lobby)
+
+    return
+
+master func player_left_lobby():
+    var id: int = get_tree().get_rpc_sender_id()
+    players_in_lobby.remove(players_in_lobby.find(id))
