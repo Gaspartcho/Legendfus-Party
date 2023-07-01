e7545fc212cf35f0752d9838e4912a72b1457df3
Gaspard Deremble <g.deremble@laposte.net>
2023-07-01 15:10:19 +0200

    bvegbv

--- server/Scripts/Lobby.gd
+++ server/Scripts/Lobby.gd
@@ -21,6 +21,7 @@ var _unused
 
 
 master func player_joined_lobby():
+    print("test")
     var id: int = get_tree().get_rpc_sender_id()
     players_in_lobby.append(id)
     rpc_id(id, "get_lobby_info", players_in_lobby)
